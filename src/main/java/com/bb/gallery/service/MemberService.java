de367ee715f574f0a5633f22bfbe48d8dc559936
NJbblack <bblackhall@naver.com>
2023-04-06 08:51:28 +0900

    me

--- src/main/java/com/bb/gallery/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.bb.gallery.service;
-
-import com.bb.gallery.dto.MemberDto;
-
-public interface MemberService {
-  public int insertMember(MemberDto memberDto);
-
-  public MemberDto loginMember(MemberDto memberDto);
-}
